0275ae51a1350bd7bdfc076dc9333551cb01c026
sudelitok <sudeelitok@hotmail.com>
2025-04-13 19:34:21 +0300

    readFileAndCreateFrames fonksiyonu eklendi

--- sender.cpp
+++ sender.cpp
@@ -1,9 +1,16 @@
 #include "sender.h"
 #include "protocol_utils.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <bitset>
+
+#define FRAME_SIZE 100
+
+using namespace std;
 
 class Sender {
 private:
-    vector<Frame> frames;
     string checksumValue;
     string checksumHex;
     bool allFramesSent;
@@ -12,13 +19,57 @@ public:
     Sender() : allFramesSent(false) {}
     
     // dosyadan veri okuma ve çerçevelere bölme işlemi
-    bool readDataFile(const string& filename) {
+    bool readFileAndCreateFrames(const string &fileName) {
+        int i;
+        ifstream file;
+        vector<string> frames;  
+        string currentFrame;  
+        char byte;
         
+        file.open(fileName.c_str(), ios::binary); 
+    
+        if (!file) {
+            cerr << "Dosya acilamadi!" << endl;
+            return false;
+        }  
+    
+        while (file.read(&byte, 1)) {
+            bitset<8> bits(byte); 
+            
+            for (i = 7; i >= 0; i--) {
+                if(bits[i]){
+                    currentFrame.push_back('1');
+                }
+                else{
+                    currentFrame.push_back('0');
+                }
+            }
+    
+            if (currentFrame.size() >= FRAME_SIZE) {
+                frames.push_back(currentFrame.substr(0, FRAME_SIZE)); 
+                currentFrame = currentFrame.substr(FRAME_SIZE); 
+            }
+        }
+    
+        if (!currentFrame.empty()) {  //son frame tamamlanmamışsa
+            while (currentFrame.size() < FRAME_SIZE) {
+                currentFrame.push_back('0');  // eksik kalan bitler 0 ile tamamlanır
+            }
+            frames.push_back(currentFrame);
+            
+        }
+    
+        for (i = 0; i < frames.size(); i++) {
+            cout << "Frame " << i + 1 << ": " << frames[i] << endl;
+        }
+    
+        file.close();
+        return true;  
     }
     
     // frame gönderme, crc-checksum işlemleri ve simülasyon işlemleri (yüzdeliklerle belirteceğimiz şeyler)
     void sendFrames(Receiver& receiver) {
-        
+    }
 };
 
 int main() {
@@ -27,10 +78,12 @@ int main() {
     // gui eklenir
     Sender sender;
     Receiver receiver;
+
+    string filename;
+    cout << "Dosya adini girin: ";
+    cin >> filename;
     
-    string filename = "test.dat"; // GUI'de kullanıcı seçecek
-    
-    if(sender.readDataFile(filename)) {
+    if(sender.readFileAndCreateFrames(filename)) {
         sender.sendFrames(receiver);
     }
     
